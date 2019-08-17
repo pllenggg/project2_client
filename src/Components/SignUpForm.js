34a0cf891580dd159b0c481bb198b47a93861d97
amypumanee <amypumanee@gmail.com>
2019-08-17 23:04:28 +1000

    add sign up

--- src/Components/SignUpForm.js
+++ src/Components/SignUpForm.js
@@ -1,17 +1,67 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-class UserForm extends Component {
+const SERVER_URL = 'http://localhost:3000/users.json';
+
+class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      email: "",
+      password: "",
+      // user_type: 'customer'
     };
+    this._handleSubmit = this._handleSubmit.bind(this);
+    this._handleInputName = this._handleInputName.bind(this);
+    this._handleInputEmail = this._handleInputEmail.bind(this);
+    this._handleInputPassword = this._handleInputPassword.bind(this);
+  }
+  _handleSubmit(event) {
+    event.preventDefault();
+    axios.post(SERVER_URL, { 
+      email: this.state.email, 
+      password: this.state.password, 
+      user_type: this.state.user_type }).then((result) => {
+        console.log(result);
+    });
   }
 
+  _handleInputName (event) {
+    this.setState({Name: event.target.value})
+  }
+  _handleInputEmail (event) {
+    this.setState({email: event.target.value})
+  }
+  _handleInputPassword (event) {
+    this.setState({password: event.target.value})
+  }
   render() {
     return (
-      <h1>This user form, if you don't like the name. No worry just change it.</h1>
+      <div>
+        <h1>Sign up</h1>
+        <form onSubmit={this._handleSubmit}>
+        <label>
+            Name:
+            <input name="name" type="text" placeholder="type your name" required onInput={ this._handleInputName } defaultValue=""/>
+          </label>
+          <label>
+            Email:
+            <input name="email" type="text" placeholder="type your email" required onInput={ this._handleInputEmail } defaultValue=""/>
+          </label>
+          <label>
+            Password:
+            <input name="password" type="password" placeholder="type your password" required onInput={ this._handleInputPassword } defaultValue=""/>
+          </label>
+          <label>
+            Confirm your password:
+            <input name="password" type="password" placeholder="type your password" required onInput={ this._handleInputPassword } defaultValue=""/>
+          </label>
+          <input type="submit" value="Sign up" />
+          <br />
+        </form>
+      </div>
     );
   }
 }
 
-export default UserForm;
+export default SignUp;
