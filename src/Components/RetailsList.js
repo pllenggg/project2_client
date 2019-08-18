784fc34ba70230e10fb97b572f132398a6018f5b
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-08-18 20:50:23 +1000

    created retails_list

--- src/Components/RetailsList.js
+++ src/Components/RetailsList.js
@@ -1,17 +1,60 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
+const RETAILS_LIST_API = 'http://localhost:3000/retails.json';
 
 class RetailsList extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      retails: []
+
     };
+
+    const fetchRetailsList = () => {
+      axios.get(RETAILS_LIST_API).then((result) => {
+        console.log(result.data);
+        this.setState({ retails: result.data });
+      });
+    };
+    fetchRetailsList();
   }
+
 
   render() {
     return (
-        <h1>This retail list form, if you don't like the name. No worry just change it.</h1>
+
+      <div>
+
+        <Button className="newRetail" variant="light" size="lg" active>Add New Retail
+        </Button>
+
+        <Table striped bordered hover>
+          <tr>
+            <th>Name</th>
+            <th>Address1</th>
+            <th>Address2</th>
+            <th>Suburb</th>
+            <th>Postcode</th>
+          </tr>
+          {this.state.retails.map((retails) => (
+            <tr>
+              <td>{retails.retail_name}</td>
+              <td>{retails.address1}</td>
+              <td>{retails.address2}</td>
+              <td>{retails.suburb}</td>
+              <td>{retails.postcode}</td>
+
+            </tr>
+          ))}
+        </Table>
+      </div>
+
     );
   }
 }
 
 export default RetailsList;
+
