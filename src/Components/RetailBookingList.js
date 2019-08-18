88e7655f8b6eb701381d5a854ba60f2c6fb11d1d
Ratsamee <jayin@hotmail.com>
2019-08-18 16:27:10 +1000

    add retail booking list

--- src/Components/RetailBookingList.js
+++ src/Components/RetailBookingList.js
@@ -1,15 +1,57 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Table } from "react-bootstrap";
+const BOOKING_SERVER_URL = "http://localhost:3000/bookings.json";
 
 class RetailBookingList extends Component {
-  constructor(props) {
-    super(props);
+  
+  constructor() {
+    super();
     this.state = {
+      booking_data: []
     };
+
+    const getBookingData = () =>{
+      axios.get(BOOKING_SERVER_URL).then((result)=>{
+        this.setState({booking_data:result.data});
+      });
+    }
+    getBookingData();
   }
 
   render() {
     return (
-        <h1>This retail booking list form, if you don't like the name. No worry just change it.</h1>
+        <div>
+          <Table striped bordered size="sm">
+            <thead>
+              <tr>
+                <th>Customer</th>
+                <th>Booking Date</th>
+                <th>Booking Time</th>
+                <th>Service</th>
+                <th>Duration</th>
+                <th>Price</th>
+                <th>Phone Number</th>
+              </tr>
+            </thead>
+            {
+              // TODO: use login id instead of 8
+              this.state.booking_data.filter((r)=>{return r.retail_id === 8}).map((b)=>{
+                return(
+                  <tr>
+                    <td>{b.customer.full_name}</td>
+                    <td>{b.date}</td>
+                    <td>{b.booking_time}</td>
+                    <td>{b.service.title}</td>
+                    <td>{b.service.duration}</td>
+                    <td>{b.service.price}</td>
+                    <td>{b.customer.phone}</td>
+                  </tr>
+                );
+              })
+            }
+          </Table>
+        </div>
     );
   }
 }
