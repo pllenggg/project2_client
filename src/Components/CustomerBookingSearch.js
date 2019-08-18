65ec06c515653c500a6829967b6fda1c087930e3
amypumanee <amypumanee@gmail.com>
2019-08-18 16:22:52 +1000

    CustomerBookingList

--- src/Components/CustomerBookingSearch.js
+++ src/Components/CustomerBookingSearch.js
@@ -4,12 +4,15 @@ class CustomerBookingSearch extends Component {
   constructor(props) {
     super(props);
     this.state = {
+
     };
   }
 
   render() {
     return (
-        <h1>This customer booking search form, if you don't like the name. No worry just change it.</h1>
+        <div>
+        <h1>Show customer Booking Search</h1>
+        </div>
     );
   }
 }
