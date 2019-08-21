8727596017b5fc964037113b2ecec83e7ade769a
pllenggg <plengna@gmail.com>
2019-08-21 14:37:30 +1000

    requirement fixed

--- src/Components/RetailForm.js
+++ src/Components/RetailForm.js
@@ -7,27 +7,27 @@ class RetailForm extends Component {
         <Form onSubmit={this.props.onSubmit}>
         <Form.Group controlId="formGridAddress1">
           <Form.Label>Name</Form.Label>
-          <Form.Control placeholder="Retail Name" name="retail_name" value={this.props.service.retail_name} onChange={this.props.onEditing} required maxLength="100" required/>
+          <Form.Control placeholder="Retail Name" name="retail_name" value={this.props.service.retail_name} onChange={this.props.onEditing} required maxLength="100" />
         </Form.Group>
 
         <Form.Group controlId="formGridAddress1">
           <Form.Label>Address 1</Form.Label>
-          <Form.Control placeholder="Unit" name="address1" value={this.props.service.address1} onChange={this.props.onEditing} required maxLength="100" required/>
+          <Form.Control placeholder="Unit" name="address1" value={this.props.service.address1} onChange={this.props.onEditing} required maxLength="100" />
         </Form.Group>
 
         <Form.Group controlId="formGridAddress2">
           <Form.Label>Address 2</Form.Label>
-          <Form.Control placeholder="Street" name="address2" value={this.props.service.address2} onChange={this.props.onEditing} required maxLength="100" required/>
+          <Form.Control placeholder="Street" name="address2" value={this.props.service.address2} onChange={this.props.onEditing} required maxLength="100" />
         </Form.Group>
 
         <Form.Group controlId="formGridAddress2">
           <Form.Label>Suburb</Form.Label>
-          <Form.Control placeholder="Surbub" name="suburb" value={this.props.service.suburb} onChange={this.props.onEditing} required maxLength="100" required/>
+          <Form.Control placeholder="Surbub" name="suburb" value={this.props.service.suburb} onChange={this.props.onEditing} required maxLength="100" />
         </Form.Group>
 
         <Form.Group controlId="formGridAddress2">
           <Form.Label>Postcode</Form.Label>
-          <Form.Control placeholder="Postcode" name="postcode" value={this.props.service.postcode} onChange={this.props.onEditing} required maxLength="100" required/>
+          <Form.Control placeholder="Postcode" name="postcode" value={this.props.service.postcode} onChange={this.props.onEditing} required maxLength="4" />
         </Form.Group>
 
         <Form.Group controlId="formGridAddress2">
@@ -42,12 +42,12 @@ class RetailForm extends Component {
 
         <Form.Group controlId="formGridAddress2">
           <Form.Label>Phone</Form.Label>
-          <Form.Control placeholder="Phone" name="phone" value={this.props.service.phone} onChange={this.props.onEditing} onChange={this.props.onEditing} />
+          <Form.Control placeholder="Phone" name="phone" value={this.props.service.phone} onChange={this.props.onEditing} onChange={this.props.onEditing} required maxLength="15"/>
         </Form.Group>
 
         <Form.Group controlId="formGridAddress2">
           <Form.Label>Website</Form.Label>
-          <Form.Control placeholder="website" name="website" value={this.props.service.website} onChange={this.props.onEditing}/>
+          <Form.Control placeholder="website" name="website" value={this.props.service.website} onChange={this.props.onEditing} required maxLength="100"/>
         </Form.Group>
 
         <Button variant="primary" type="submit">Submit</Button>
