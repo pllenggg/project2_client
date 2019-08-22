24591389b0ea2e2e9c233e7999839c886b29868c
amypumanee <amypumanee@gmail.com>
2019-08-22 12:03:41 +1000

    css in process

--- src/Components/CustomerBookingSearch.js
+++ src/Components/CustomerBookingSearch.js
@@ -88,10 +88,11 @@ class SearchForm extends Component {
                   value={this.state.suburb}
                   required />
               </Form.Group>
+              <Col>
+              <Button id="search" type="submit">Search</Button>
+            </Col>
             </Form.Row>
-            <Button variant="primary" type="submit">
-              Search
-            </Button>
+
           </form>
         </Container>
       </div>
@@ -114,7 +115,7 @@ class SearchResult extends Component {
                       <Card.Title className="titleCategory">{s.title}</Card.Title>
                       <Card.Text>Description: {s.description} </Card.Text>
                       <Card.Text>Service price: ${s.price}</Card.Text>
-                      <Card.Text>Retail: {s.retail.retail_name}</Card.Text>
+                      <Card.Text><strong> {s.retail.retail_name} </strong></Card.Text>
                       <Card.Text><span role="img" aria-label="sheep">📍</span>{s.retail.address1}, {s.retail.address2}, {s.retail.suburb}</Card.Text>
                       <Card.Text><span role="img" aria-label="sheep">☎️</span> {s.retail.phone}</Card.Text>
                       <Button variant="outline-info" href={`#/retailshowservices/${s.retail.user_id}`}>
